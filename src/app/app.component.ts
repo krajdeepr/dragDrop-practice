fe0b448eb0d74950d8473f492308617a055f1c17
Rajdeep <rajdeep_katamreddy@epam.com>
2020-01-03 16:53:32 +0530

    drag and drop functionality done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,64 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, DoCheck } from '@angular/core';
+import {CdkDragDrop, moveItemInArray, transferArrayItem} from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements DoCheck{
   title = 'dragDrop-practice';
+  initialAvailableHeaders:string[];
+  initialColumns:string[];
+  initialRows:string[];
+
+  availableHeaders = [
+    'Series & Expression Names',
+    'Currency',
+    'Date(Qn YYYY format)'
+  ];
+
+  columns = [
+    'Start date',
+    'End date',
+    'Frequency',
+    'Series Name',
+    'Series Code'
+  ];
+
+  rows=[
+    'Date (locale format)'
+  ];
+  ngDoCheck(){
+  this.initialAvailableHeaders=[
+    'Series & Expression Names',
+    'Currency',
+    'Date(Qn YYYY format)'
+  ];
+  this.initialColumns=[
+    'Start date',
+    'End date',
+    'Frequency',
+    'Series Name',
+    'Series Code'
+  ];
+  this.initialRows=[
+    'Date (locale format)'
+  ];
+}
+  drop(event: CdkDragDrop<string[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    } else {
+      transferArrayItem(event.previousContainer.data,
+                        event.container.data,
+                        event.previousIndex,
+                        event.currentIndex);
+    }
+  }
+  reset(){
+    this.availableHeaders=this.initialAvailableHeaders;
+    this.columns=this.initialColumns;
+    this.rows=this.initialRows;
+  }
 }
---
+++ src/app/data.ts
@@ -0,0 +1,76 @@
+export default[
+    {
+      "Id": "1",
+      "Group_Id": "1",
+      "Name": "Series & Expression Names",
+      "availableHeaders": true,
+      "columns": true,
+      "rows": true
+
+    },
+    {
+      "Id": "2",
+      "Group_Id": "1",
+      "Name": "Currency",
+      "availableHeaders": true,
+      "columns": true,
+      "rows": true
+    },
+    {
+      "Id": "3",
+      "Group_Id": "1",
+      "Name": "Date(Qn YYYY format)",
+      "availableHeaders": true,
+      "columns": false,
+      "rows": true
+    },
+    {
+      "Id": "4",
+      "Group_Id": "2",
+      "Name": "Start date",
+      "availableHeaders": true,
+      "columns": true,
+      "rows": false
+    },
+    {
+      "Id": "5",
+      "Group_Id": "2",
+      "Name": "End date",
+      "availableHeaders": true,
+      "columns": true,
+      "rows": true
+    },
+    {
+        "Id": "6",
+        "Group_Id": "2",
+        "Name": "Frequency",
+        "availableHeaders": false,
+        "columns": true,
+        "rows": false
+      },
+      {
+        "Id": "7",
+        "Group_Id": "2",
+        "Name": "Series Name",
+        "availableHeaders": true,
+        "columns": true,
+        "rows": true
+      },
+      {
+        "Id": "8",
+        "Group_Id": "2",
+        "Name": "Series Code",
+        "availableHeaders": false,
+        "columns": true,
+        "rows": true
+      },
+      {
+        "Id": "9",
+        "Group_Id": "3",
+        "Name": "Date (locale format)",
+        "availableHeaders": false,
+        "columns": true,
+        "rows": true
+      },
+
+]
