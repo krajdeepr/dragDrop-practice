103c2b6d3054c7292e165a0ed36c003f165ecf20
Rajdeep <rajdeep_katamreddy@epam.com>
2020-01-06 11:21:52 +0530

    drag disability added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,64 +1,72 @@
-import { Component, OnInit, DoCheck } from '@angular/core';
-import {CdkDragDrop, moveItemInArray, transferArrayItem} from '@angular/cdk/drag-drop';
+import { Component, OnInit } from "@angular/core";
+import {
+  CdkDragDrop,
+  moveItemInArray,
+  transferArrayItem
+} from "@angular/cdk/drag-drop";
+import data from "./data";
 
-@Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
-})
-export class AppComponent implements DoCheck{
-  title = 'dragDrop-practice';
-  initialAvailableHeaders:string[];
-  initialColumns:string[];
-  initialRows:string[];
-
-  availableHeaders = [
-    'Series & Expression Names',
-    'Currency',
-    'Date(Qn YYYY format)'
-  ];
-
-  columns = [
-    'Start date',
-    'End date',
-    'Frequency',
-    'Series Name',
-    'Series Code'
-  ];
-
-  rows=[
-    'Date (locale format)'
-  ];
-  ngDoCheck(){
-  this.initialAvailableHeaders=[
-    'Series & Expression Names',
-    'Currency',
-    'Date(Qn YYYY format)'
-  ];
-  this.initialColumns=[
-    'Start date',
-    'End date',
-    'Frequency',
-    'Series Name',
-    'Series Code'
-  ];
-  this.initialRows=[
-    'Date (locale format)'
-  ];
+interface KeyValuePair {
+  Name: string;
+  disabled: boolean;
 }
+@Component({
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
+})
+export class AppComponent implements OnInit {
+  title = "dragDrop-practice";
+  availableHeaders: KeyValuePair[] = [];
+  columns: KeyValuePair[] = [];
+  rows: KeyValuePair[] = [];
+  ngOnInit() {
+    this.data();
+  }
+  data() {
+    data.forEach(item => {
+      switch (item.Group_Id) {
+        case "1":
+          this.availableHeaders.push({
+            Name: item.Name,
+            disabled: item.disabled
+          });
+          break;
+        case "2":
+          this.columns.push({
+            Name: item.Name,
+            disabled: item.disabled
+          });
+          break;
+        case "3":
+          this.rows.push({
+            Name: item.Name,
+            disabled: item.disabled
+          });
+          break;
+      }
+    });
+  }
   drop(event: CdkDragDrop<string[]>) {
     if (event.previousContainer === event.container) {
-      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+      moveItemInArray(
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex
+      );
     } else {
-      transferArrayItem(event.previousContainer.data,
-                        event.container.data,
-                        event.previousIndex,
-                        event.currentIndex);
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex
+      );
     }
   }
-  reset(){
-    this.availableHeaders=this.initialAvailableHeaders;
-    this.columns=this.initialColumns;
-    this.rows=this.initialRows;
+  reset() {
+    this.availableHeaders = [];
+    this.columns = [];
+    this.rows = [];
+    this.data();
   }
 }
--- src/app/data.ts
+++ src/app/data.ts
@@ -1,76 +1,56 @@
-export default[
-    {
-      "Id": "1",
-      "Group_Id": "1",
-      "Name": "Series & Expression Names",
-      "availableHeaders": true,
-      "columns": true,
-      "rows": true
-
-    },
-    {
-      "Id": "2",
-      "Group_Id": "1",
-      "Name": "Currency",
-      "availableHeaders": true,
-      "columns": true,
-      "rows": true
-    },
-    {
-      "Id": "3",
-      "Group_Id": "1",
-      "Name": "Date(Qn YYYY format)",
-      "availableHeaders": true,
-      "columns": false,
-      "rows": true
-    },
-    {
-      "Id": "4",
-      "Group_Id": "2",
-      "Name": "Start date",
-      "availableHeaders": true,
-      "columns": true,
-      "rows": false
-    },
-    {
-      "Id": "5",
-      "Group_Id": "2",
-      "Name": "End date",
-      "availableHeaders": true,
-      "columns": true,
-      "rows": true
-    },
-    {
-        "Id": "6",
-        "Group_Id": "2",
-        "Name": "Frequency",
-        "availableHeaders": false,
-        "columns": true,
-        "rows": false
-      },
-      {
-        "Id": "7",
-        "Group_Id": "2",
-        "Name": "Series Name",
-        "availableHeaders": true,
-        "columns": true,
-        "rows": true
-      },
-      {
-        "Id": "8",
-        "Group_Id": "2",
-        "Name": "Series Code",
-        "availableHeaders": false,
-        "columns": true,
-        "rows": true
-      },
-      {
-        "Id": "9",
-        "Group_Id": "3",
-        "Name": "Date (locale format)",
-        "availableHeaders": false,
-        "columns": true,
-        "rows": true
-      },
-
-]
+export default [
+  {
+    Id: "1",
+    Group_Id: "1",
+    Name: "Series & Expression Names",
+    disabled: false
+  },
+  {
+    Id: "2",
+    Group_Id: "1",
+    Name: "Currency",
+    disabled: false
+  },
+  {
+    Id: "3",
+    Group_Id: "1",
+    Name: "Date(Qn YYYY format)",
+    disabled: true
+  },
+  {
+    Id: "4",
+    Group_Id: "2",
+    Name: "Start date",
+    disabled: true
+  },
+  {
+    Id: "5",
+    Group_Id: "2",
+    Name: "End date",
+    disabled: false
+  },
+  {
+    Id: "6",
+    Group_Id: "2",
+    Name: "Frequency",
+    disabled: false
+  },
+  {
+    Id: "7",
+    Group_Id: "2",
+    Name: "Series Name",
+    disabled: true
+  },
+  {
+    Id: "8",
+    Group_Id: "2",
+    Name: "Series Code",
+    disabled: false
+  },
+  {
+    Id: "9",
+    Group_Id: "3",
+    Name: "Date (locale format)",
+    disabled: false
+  }
+];
